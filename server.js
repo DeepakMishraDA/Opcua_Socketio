a34c1a693918d8481aa5830233985e2f9fc89ad4
deepak <deepakvinodmishra@gmail.com>
2022-12-04 22:26:01 +0100

    httpServer added

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const { Server } = require("socket.io");
+
+const alarmsOpc = require('./opcua/alarmApp');
+
+const reqResFunc = (req,res) => {
+    if(req.method = 'GET') {
+        res.writeHead(200);
+        res.end("All Ok!")
+    }
+}
+//const server = () =>{
+
+//};
+
+const serv = http.createServer(reqResFunc);
+const io = new Server(serv);
+
+
+
+//server()
+serv.listen(5000);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    alarmsOpc(socket)
+
+    socket.on('conn',data=>{
+        console.log(data)})
+  });

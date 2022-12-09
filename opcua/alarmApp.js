0c79b74326141395dcd482ef40f50c3fdbb22a8e
deepak <deepakvinodmishra@gmail.com>
2022-12-09 18:28:48 +0100

    yarn.lock added

--- opcua/alarmApp.js
+++ opcua/alarmApp.js
@@ -5,7 +5,6 @@ async function alarmsOpc(socket) { //{endpoint,socket}
     const alarmsInstance =  await getAlarms(process.env.EndpointUrl);
     alarmsInstance.on('newAlarm', async alarmValue => {
         try {   
-          //console.log(alarmValue.fields.message);
           socket.emit('alarms',alarmValue.fields.conditionId.value)
         } catch (err) {
           console.log(err);
@@ -13,7 +12,6 @@ async function alarmsOpc(socket) { //{endpoint,socket}
       });
       alarmsInstance.on('alarmChanged', async alarmValue => {
         try {
-          //console.log(alarmValue.fields.message);
           socket.emit('alarmsc',alarmValue.fields.conditionId.value)
         } catch (err) {
           console.log(err);

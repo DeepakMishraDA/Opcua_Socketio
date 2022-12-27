cba8d95e1f4d718acbcfdae9fe70e45c4446b271
deepak <deepakvinodmishra@gmail.com>
2022-12-27 20:13:39 +0100

    listening on 3000

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const alarmsOpc = require('./opcua/alarmApp');
+require('dotenv').config();
 
 app.get('/',(req,res)=>{
     res.attachment(path.resolve('./dummy.txt'));  
@@ -32,7 +33,7 @@ io.on('connection', (socket) => {
 //   console.log('Listening on port 3000');
 // });
 
-const port = 3000;
+const port = process.env.Port;
 
 app.listen(port,() => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);

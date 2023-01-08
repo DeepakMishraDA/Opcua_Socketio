8c56728b884218744ef9cca982965bb1fd9f6177
deepak <deepakvinodmishra@gmail.com>
2023-01-08 05:09:41 +0100

    removed commented codes

--- server.js
+++ server.js
@@ -22,14 +22,4 @@ io.on('connection', (socket) => {
     socket.on('conn',data=>{
         console.log(data)})
   });
-
-  // let opcIp = buildingsList.find(element => element.id == data.buildingId)
-            // const devOrProd = jwt_decode(socket.handshake.headers.cookie)
-            // if((devOrProd.iss == 'https://dev.green-fusion.de/auth/realms/development') && 
-            //   (opcIp.plc_ip == 'opc.tcp://10.0.42.1:4840')){
-            //         opcIp = 'opc.tcp://10.0.42.2:4840'
-            // }
-            // console.log(":_)#))$",opcIp)
-
-
-            //const jwt_decode = require('jwt-decode');
+  

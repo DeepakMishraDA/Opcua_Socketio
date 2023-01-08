7f83bf15a616c109bd50cdf4531e48df2d082a1d
deepak <deepakvinodmishra@gmail.com>
2023-01-08 05:09:01 +0100

    getalarms modified resulting ok only

--- opcua/getAlarms.js
+++ opcua/getAlarms.js
@@ -1,10 +1,24 @@
 const  { OPCUAClient } = require('node-opcua');
 const alarmCaller = require('./alarmCaller');
+const beforeShutdown = require('./beforeShutdown');
 
   async function opcuaSession({ endpointUrl }) {
     const client = OPCUAClient.create({
       endpointMustExist: false,
     });
+     client.on('backoff', (retry, delay) => {
+      console.log('Retrying to connect to ', endpointUrl, ' attempt ', retry);
+    });
+     client.on('abort', () => {
+      process.exit(0);
+    });
+     client.on('connection_lost', () => {
+      process.exit(0);
+    });
+     client.on('timed_out_request', () => {
+      process.exit(0);
+    });
+    console.log('connecting to endpoint');
     if (endpointUrl) {
       await client.connect(endpointUrl);
     }
@@ -17,7 +31,12 @@ const alarmCaller = require('./alarmCaller');
       publishingEnabled: true,
       priority: 10,
     });
-
+    (async () => {
+      console.log('closing opcua session');
+      await this.session.close(true);
+      await this.client.disconnect();
+      console.log('opcua session closed');
+    });
     return session;
   }
 
@@ -30,4 +49,4 @@ async function getAlarms(endpointUrl) {
     return  alarmInstance;
   }
 
-module.exports = getAlarms;
+module.exports = {getAlarms,opcuaSession} ;

3699edb375b5b009602c372f61ba811a89ab8b67
deepak <deepakvinodmishra@gmail.com>
2023-02-23 10:51:09 +0100

    changes in alarms app itself trial

--- opcua/alarmApp.js
+++ opcua/alarmApp.js
@@ -1,22 +1,29 @@
+const carryQuery = require('../database/carryQuery');
 const {getAlarms} = require('./getAlarms');
 require('dotenv').config();
 
-async function alarmsOpc(socket) { //{endpoint,socket}
-    const alarmsInstance =  await getAlarms(process.env.EndpointUrl);
-    console.log(alarmsInstance)
-    //socket.emit('alarms',alarmsInstance)
-    alarmsInstance.on('newAlarm', async alarmValue => {
-      //console.log(alarmValue)
+async function alarmsOpc(socket,data) { //{endpoint,socket}
+  console.log(data)
+    const alarmsInstance =  await getAlarms(data);
+    //console.log(alarmsInstance)
+    alarmsInstance.on('newAlarm', async alarmValue => {   
+      //console.log("?:",alarmValue)
+      socket.emit('alarms',alarmValue.fields)
+     // await carryQuery(alarmValue)
         try {   
-          //socket.emit('alarms',alarmValue)
-          //console.log(alarmValue)
+         // await carryQuery({alarm:alarmValue})
+          socket.emit('alarms',alarmValue)
         } catch (err) {
           console.log(err);
         }
       });
       alarmsInstance.on('alarmChanged', async alarmValue => {
+     
         try {
-           //socket.emit('alarmsc',alarmValue.fields.conditionId.value)
+          if (alarmValue){
+        //  await carryQuery(alarmValue)
+          }
+          // socket.emit('alarmsc',alarmValue.fields.conditionId.value)
         } catch (err) {
           console.log(err);
         }

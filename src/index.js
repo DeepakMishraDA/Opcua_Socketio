5ed1a9a7c01d0851aed672c748ff29c6f6c9c762
deepak <deepakvinodmishra@gmail.com>
2023-02-26 17:10:56 +0100

    modified socket endpoints in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+var json = require('../providers/alarmdata.json');
+const  users = require('../routers/alarmRoute');
+// var { buildSchema } = require('graphql');
 var router = express.Router();
 const path = require('path')
 const app = express();
@@ -13,20 +14,22 @@ const server = http.createServer(app);
 const io = require('socket.io')(server);
 const alarmsOpc = require('../opcua/alarmApp');
 require('dotenv').config();
-
+app.set('port', process.env.PORT || 3000)
 app.get('/',(req,res)=>{
     res.attachment(path.resolve('./dummy.txt'));  
     res.send();
        console.log(req.params);
     }
 ); 
+app.use('/users',users)
+
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
   socket.on('alarms', (data) => {
     console.log(`Received data: ${data}`);
-    alarmsOpc(socket)
+    alarmsOpc(socket,data)
   });
 
   socket.on('disconnect', () => {
@@ -36,9 +39,9 @@ io.on('connection', (socket) => {
 
 
 
-const port = process.env.Port;
+const port = process.env.PORT;
 
-server.listen(port,() => {
+server.listen(app.get('port'),() => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
   });
 

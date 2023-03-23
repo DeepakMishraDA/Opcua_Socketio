84bb9b33e0cffb887de0b6a8e33d3413cb311057
deepak <deepakvinodmishra@gmail.com>
2023-03-23 10:43:42 +0100

    altered opc ips

--- providers/socketClient.js
+++ providers/socketClient.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 
 const client = io.connect('http://localhost:5000');
 
-//const data = 'opc.tcp://10.0.42.2:4840';
-const data1 = 'opc.tcp://10.0.42.1:4840';
-//const data2 = 'opc.tcp://10.6.3.1:4840'
-//client.emit('conn',data)
-client.emit('conn1',data1)
+const data = 'opc.tcp://10.0.42.2:4840';
+//const data1 = 'opc.tcp://10.0.42.1:4840';
+const data2 = 'opc.tcp://10.6.3.1:4840';
+//client.emit('conn',data);
+client.emit('conn1',data)
 
 client.on('alarms',data =>{
         const fields = Object.keys(data)
